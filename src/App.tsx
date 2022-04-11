c6040c96c07ec6ee85c24bfb38a60f337b94646f
rheber <robert@spiff.com.au>
2022-04-11 16:03:25 +1000

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import GraphiQL from 'graphiql';
+import 'graphiql/graphiql.min.css';
+import {ChangeEventHandler, useCallback, useState} from 'react';
 
 function App() {
+  const exampleUrl = "http://localhost:1234/graphql"
+  const [url, setUrl] = useState(exampleUrl);
+  const [urlField, setUrlField] = useState(exampleUrl);
+
+  const fetcher = useCallback(async (graphQLParams) => {
+    if (!url) {
+      return "";
+    }
+    const data = await fetch(
+      url,
+      {
+        method: 'POST',
+        headers: {
+          Accept: 'application/json',
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(graphQLParams),
+        credentials: 'same-origin',
+      },
+    );
+    return data.json().catch(() => data.text());
+  }, [url]);
+
+  const handleUrlInput: ChangeEventHandler<HTMLInputElement> = useCallback((e) => {
+    setUrlField(e.target.value);
+  }, []);
+
+  const handleUrlButton = useCallback(() => {
+    setUrl(urlField);
+  }, [urlField]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{height: '100%', display: 'flex', flexDirection: 'column'}}>
+      <div>
+        <label>GraphQL endpoint</label>
+        <button onClick={handleUrlButton}>Set me!</button>
+        <input value={urlField} onChange={handleUrlInput} style={{width: '50%'}} />
+      </div>
+      <GraphiQL
+        fetcher={fetcher}
+      />
     </div>
   );
 }
